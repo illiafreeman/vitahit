d69244c624de0c909bd558a4f02800b35fc786c7
freeman <iviewn@gmail.com>
2022-12-12 01:05:40 +0300

    index

--- js/custom.js
+++ js/custom.js
@@ -2,17 +2,22 @@ $(document).ready(function () {
 
     /*top menu toggle*/
 
-    $('.header-menu__btn').click(function () {
-        $('.header-navgroup').toggleClass('vis');
+    $('.header__btn').click(function () {
+        $(this).toggleClass('close');
+        $('.nav').toggleClass('vis');
         $('body').toggleClass('oh');
         $('#hamburger').toggleClass('open');
+        $('li.vis').removeClass('vis');
     });
-    $('.menu').click(function () {
-        $('.header-navgroup').removeClass('vis');
-        $('body').removeClass('oh');
-        $('#hamburger').removeClass('open');
+    
+    $('.nav > li > a').on('click', function (e) {
+        e.preventDefault();
+        $(this).parent('li').toggleClass('vis');
     });
-
+    $('.nav > li > ul > li> a').on('click', function (e) {
+        e.preventDefault();
+        $(this).parent('li').toggleClass('vis');
+    });
     /*end top menu toggle*/
 
     /*scroll to top*/
@@ -87,47 +92,9 @@ $(document).ready(function () {
         ]
     });
 });
-$(window).on("load", function () {
 
-    new WOW().init();
 
-});
-$('.header-nav__link').on('click', function (event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top - 10
-    }, 1500, 'easeInOutExpo');
-    remove_hash_from_url();
-    $('.header-navgroup').removeClass('vis');
-    $('body').removeClass('oh');
-    $('#nav-icon4').removeClass('open');
-    event.preventDefault();
-    //remove_hash_from_url();
-});
-function remove_hash_from_url() {
-    var uri = window.location.toString();
 
-    if (uri.indexOf("#") > 0) {
-        var clean_uri = uri.substring(0,
-            uri.indexOf("#"));
-
-        window.history.replaceState({},
-            document.title, clean_uri);
-    }
-}
-
-if ($(window).width() > 1280) {
-    document.addEventListener("mousemove", parallax);
-    function parallax(event) {
-        this.querySelectorAll(".move").forEach((shift) => {
-            const position = shift.getAttribute("value");
-            const x = (window.innerWidth - event.pageX * position) / 400;
-            const y = (window.innerHeight - event.pageY * position) / 100;
-
-            shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
-        });
-    }
-}
 
 
 
